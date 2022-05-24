e7d03753a220780fd53fdc76ad0ab0ddf272883a
andreeabucur1 <andreea.bucur@my.fmi.unibuc.ro>
2022-05-24 23:48:29 +0300

    Add property posts pages and filters

---
+++ src/app/shared/pipes/filter-property-posts.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FilterPropertyPostsPipe } from './filter-property-posts.pipe';
+
+describe('FilterPropertyPostsPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FilterPropertyPostsPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ src/app/shared/pipes/filter-property-posts.pipe.ts
@@ -0,0 +1,43 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { min } from 'rxjs';
+import { PropertyPost } from 'src/app/property-posts/property.post';
+
+@Pipe({
+  name: 'filterPropertyPosts'
+})
+export class FilterPropertyPostsPipe implements PipeTransform {
+
+  transform(propertyPosts: PropertyPost[], city: string, numberOfRooms: number, numberOfBathrooms: number, minSurface: number, minYear: number | undefined, hasElevator: boolean | undefined, hasParkingSpot: boolean | undefined, propertyType: "Apartment" | "House" | "", maxPrice: number | undefined): PropertyPost[] {
+    console.log(city, numberOfRooms, numberOfBathrooms, minSurface, minYear, hasElevator, hasParkingSpot, propertyType, maxPrice);
+    
+    if (city && city != "All") {
+        propertyPosts = propertyPosts.filter( propertyPost => propertyPost.city?.includes(city));
+    }
+    if (numberOfRooms > 0) {
+      propertyPosts = propertyPosts.filter( propertyPost => propertyPost.numberOfRooms === numberOfRooms);
+    }
+    if (numberOfBathrooms > 0) {
+      propertyPosts = propertyPosts.filter(propertyPost => propertyPost.numberOfBathrooms === numberOfBathrooms);
+    }
+    if (minSurface > 0) {
+      propertyPosts = propertyPosts.filter(propertyPost => propertyPost.surface >= minSurface)
+    }
+    if (minYear != undefined ) {
+      propertyPosts = propertyPosts.filter(propertyPost => propertyPost.buildYear >= minYear);
+    }
+    if (hasParkingSpot !== undefined && hasParkingSpot) {
+      propertyPosts = propertyPosts.filter(propertyPost => propertyPost.hasParkingSpot === hasParkingSpot);
+    }
+    if (hasElevator !== undefined && hasElevator) {
+      propertyPosts = propertyPosts.filter(propertyPost => propertyPost.hasElevator === hasElevator);
+    }
+    if (propertyType) {
+      propertyPosts = propertyPosts.filter(propertyPost => propertyPost.propertyType === propertyType);
+    }
+    if (maxPrice) {
+      propertyPosts = propertyPosts.filter(propertyPost => propertyPost.price <= maxPrice);
+    }
+    return propertyPosts;
+  }
+
+}
